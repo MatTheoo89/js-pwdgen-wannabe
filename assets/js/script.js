b0b4203cd7b702d3a9a51e18e078a291dcd255d5
MatTheoo89 <matteobellomo89@gmail.com>
2022-10-10 14:02:51 +0200

    [add] exercize

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,31 @@ poi chiedi il suo colore preferito
 Infine scrivi sulla pagina 
 
 nomecognomecolorepreferito22*/
+
+// * Le nostre variabili
+
+let nomeUtente;
+let cognomeUtente;
+let colore;
+let myOutput;
+
+// * inizializzazione variabili
+
+nomeUtente = prompt('Inserisci il tuo nome')
+cognomeUtente = prompt('Inserisci il tuo Cognome')
+colore = prompt('Inserisci il tuo colore preferito')
+
+// ! OUTPUT
+
+myOutput = `
+  ${nomeUtente}${cognomeUtente}${colore}22
+`;
+
+document.getElementById('password').innerHTML += myOutput;
+
+// ! CONSOLE
+
+console.log(nomeUtente);
+console.log(cognomeUtente);
+console.log(colore);
+console.log(myOutput);
